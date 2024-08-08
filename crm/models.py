9a57883d80e7483b4048472bf5c5c8dc94ed73b7
jensen-dong <jensendong141@gmail.com>
2024-08-07 18:42:27 -0700

    feat: add client model

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=10)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
