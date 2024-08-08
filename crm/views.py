97118fee1a1116827094c1beb1b46b312277f053
jensen-dong <jensendong141@gmail.com>
2024-08-07 18:59:12 -0700

    feat: add client views

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Client
+from .forms import ClientForm
 
 # Create your views here.
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'crm/client_list.html', {'clients': clients})
+
+def client_create(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm()
+    return render(request, 'crm/client_form.html', {'form': form})
+
+def client_update(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'crm/client_form.html', {'form': form})
+
